003066138265e9b421f26234ae9a683f1cfca5af
satxix <avllcrt@gmail.com>
2026-06-26 23:05:06 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'villacart-pos-v5.6.26';
+const CACHE_NAME = 'villacart-pos-v5.6.27';
 const APP_SHELL = ['./', './index.html', './styles.css', './app.js', './diagnostics.js'];
 
 self.addEventListener('install', event => {
