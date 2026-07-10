c9572696f8e92ddce3f607b64b8c41b6e6304764
satxix <avllcrt@gmail.com>
2026-07-10 08:45:17 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
-const CACHE_NAME = 'villacart-pos-v7.2.43';
+const CACHE_NAME = 'villacart-pos-v7.2.45';
 const APP_SHELL = [
   './',
   './index.html',
-  './manifest.webmanifest?v=7.2.43',
-  './styles.css?v=7.2.43',
-  './app.js?v=7.2.43',
-  './diagnostics.js?v=7.2.43',
+  './manifest.webmanifest?v=7.2.45',
+  './styles.css?v=7.2.45',
+  './app.js?v=7.2.45',
+  './diagnostics.js?v=7.2.45',
   './assets/icons/icon-192.png',
   './assets/icons/icon-512.png',
   './assets/icons/maskable-512.png',
